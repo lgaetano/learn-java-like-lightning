8bfb20ae25d7dea6e2dc0c22a14b4292963ebf81
Lia Gaetano <lgaetano19@gmail.com>
2022-01-30 09:20:13 -0800

    Lesson 3 code challenge

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,38 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String args[]) {
-        String question = "";
-        String choiceOne = "";
-        String choiceTwo = "";
-        String choiceThree = "";
+        String question = "What number am I thinking of?";
+        String a = "One";
+        String b = "Twenty-four";
+        String c = "Three hundred";
 
-        String correctAnswer = choiceTwo;
+        String answer = a;
 
         // Write a print statement asking the question
+        System.out.println(question);
         // Write a print statement giving the answer choices
+        System.out.println("A) " + a);
+        System.out.println("B) " + b);
+        System.out.println("C) " + c);
 
         // Have the user input an answer
+        Scanner input = new Scanner(System.in);
         // Retrieve the user's input
-
+        String userInput = input.next();
+        
         // If the user's input matches the correctAnswer...
-        // then the user is correct and we want to print out a congrats message to the user.
+        if (userInput.equals(answer) || userInput.equals("A")) {
+            // then the user is correct and we want to print out a congrats message to the user.
+            System.out.println("CONGRATS!!! YOU WIN!!");
+        } else {
+            // If the user's input does not match the correctAnswer...
+            // then the user is incorrect and we want to print out a message saying that the user is incorrect as well as what the correct choice was.
+            System.out.println("Incorrect...");
+        }
 
-        // If the user's input does not match the correctAnswer...
-        // then the user is incorrect and we want to print out a message saying that the user is incorrect as well as what the correct choice was.
-
+        
     }
 
 }
